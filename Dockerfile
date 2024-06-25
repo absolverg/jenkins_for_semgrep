f7503a9bee8e8f75b7f0e93d4e2f808aca742527
absolverg <absolverg@gmail.com>
2024-06-25 15:27:08 +0900

    TF-CLC 버전 변경

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apt-get update && apt-get install -y python3 pipx
 COPY TF_install.sh /root/TF_install.sh
 RUN chmod +x /root/TF_install.sh
 RUN /root/TF_install.sh
-ENV PATH="/usr/local/bin/TEE-CLC-14.139.0:${PATH}"
+ENV PATH="/usr/local/bin/TEE-CLC-14.135.3:${PATH}"
 RUN tf eula -accept
 
 USER jenkins
--- TF_install.sh
+++ TF_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L https://github.com/microsoft/team-explorer-everywhere/releases/download/14.139.0/TEE-CLC-14.139.0.zip -o TEE-CLC-14.139.0.zip
-unzip TEE-CLC-14.139.0.zip
-cp -r TEE-CLC-14.139.0 /usr/local/bin
+curl -L https://github.com/JetBrains/team-explorer-everywhere/releases/download/14.135.3/TEE-CLC-14.135.3.zip -o TEE-CLC-14.135.3.zip
+unzip TEE-CLC-14.135.3.zip
+cp -r TEE-CLC-14.135.3 /usr/local/bin
