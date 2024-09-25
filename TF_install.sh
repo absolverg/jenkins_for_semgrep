7b6435c92a251203195609e4ea8c441f3c740e84
absolverg <absolverg@gmail.com>
2024-09-25 10:04:19 +0900

    Delete TF

--- TF_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -L https://github.com/JetBrains/team-explorer-everywhere/releases/download/14.135.3/TEE-CLC-14.135.3.zip -o TEE-CLC-14.135.3.zip
-unzip TEE-CLC-14.135.3.zip
-cp -r TEE-CLC-14.135.3 /usr/local/bin
